182db85b8d4fcb2ded8126d9a421b698f59daf99
David seco <deivid.develop@gmail.com>
2022-03-06 11:35:31 +0100

    Add: testing tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    minWidth: {
+      '0': '0',
+      '100': '100px',
+      '200': '200px',
+      '300': '300px',
+      '1/2': '50%',
+      'full': '100%',
+    },
+    screens: {
+      'sm': {'min': '0px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1224px' },
+
+    },
     extend: {},
   },
   plugins: [],
